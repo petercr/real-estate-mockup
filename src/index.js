28b06fb3a5f241ad81204a5e90d597f22fff4c6c
Peter <petecruckshank@gmail.com>
2018-04-28 20:30:04 -0400

    Moved <SCRIPT/>  from .html to /src/index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+//Sidenav
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+// Image Slider
+const slider = document.querySelector(".slider");
+let imageSlider = M.Slider.init(slider);
