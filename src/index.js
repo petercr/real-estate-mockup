b5db3bfb8a3db58bcdbcea28d3fec1c5b9ed9b64
Peter Cruckshank <petecruckshank@gmail.com>
2018-05-05 13:12:49 -0400

    tried to get fetch() to work with Zillow API

--- src/index.js
+++ src/index.js
@@ -34,3 +34,22 @@ M.Materialbox.init(mb, {});
 // Scrollspy
 const ss = document.querySelectorAll(".scrollspy");
 M.ScrollSpy.init(ss, {});
+
+// base url for the Zillow API
+const url = `http://www.zillow.com/webservice/GetSearchResults.htm?`;
+
+// zws-id key for calling the API
+// This is my personal key, please don't use for your own projects
+const zwsId = `zws-id=X1-ZWz1gdyz30wvt7_5rw5j&`;
+
+// basic address in Seattle copied from Zillow docs 
+let basicAddress = `address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA`;
+
+const headers = 
+
+fetch((url + zwsId + basicAddress), {mode: "no-cors"})
+  .then(req => req.json())
+  .then(function (res){
+    console.log(res);
+  })
+  .catch(error => console.error(error))
