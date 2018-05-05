5f61507d63f62cb517e50c191a8cfec9bf3b81d9
Peter Cruckshank <petecruckshank@gmail.com>
2018-05-05 12:49:42 -0400

    changed indent to 2 spaces from 4

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
-
-
 //Sidenav
-const sideNav = document.querySelector('.sidenav');
+const sideNav = document.querySelector(".sidenav");
 M.Sidenav.init(sideNav, {});
 
 // Image Slider
 const slider = document.querySelector(".slider");
 
 M.Slider.init(slider, {
-    indicators: false,
-    height: 500,
-    transition: 500,
-    interval: 4000
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 4000
 });
 
-// Autocomplete 
-const auto = document.querySelector('.autocomplete');
+// Autocomplete
+const auto = document.querySelector(".autocomplete");
 M.Autocomplete.init(auto, {
-    data: {
-        "Brewster": null,
-        "Truro": null,
-        "Centerville": null,
-        "Barnstable": null,
-        "Orleans": null,
-        "Provincetown": null,
-        "Falmouth": null,
-        "Sandwich": null,
-    }
+  data: {
+    Brewster: null,
+    Truro: null,
+    Centerville: null,
+    Barnstable: null,
+    Orleans: null,
+    Provincetown: null,
+    Falmouth: null,
+    Sandwich: null
+  }
 });
 
-// Material boxes 
-const mb = document.querySelectorAll('.materialboxed');
+// Material boxes
+const mb = document.querySelectorAll(".materialboxed");
 M.Materialbox.init(mb, {});
 
 // Scrollspy
-const ss = document.querySelectorAll('.scrollspy');
+const ss = document.querySelectorAll(".scrollspy");
 M.ScrollSpy.init(ss, {});
